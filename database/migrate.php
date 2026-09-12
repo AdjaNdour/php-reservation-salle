cceab9cd26ef32faa5f223a89bac97b9a13a7165
AdjaNdour <adjacourandour4@gmail.com>
2026-09-12 13:09:21 +0000

    correction pour le deploiement

--- database/migrate.php
+++ database/migrate.php
@@ -1,44 +1,4 @@
 <?php
-
-// declare(strict_types=1);
-
-// use DI\ContainerBuilder;
-// use Dotenv\Dotenv;
-// use Illuminate\Database\Capsule\Manager as CapsuleManager;
-
-// require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-// $dotenv = Dotenv::createImmutable(dirname(__DIR__));
-// $dotenv->load();
-
-// $builder = new ContainerBuilder();
-// $builder->useAutowiring(true);
-// $builder->addDefinitions(dirname(__DIR__) . '/config/container.php');
-// $container = $builder->build();
-// $container->get(CapsuleManager::class);
-
-// $migrations = glob(dirname(__DIR__) . '/database/migrations/*.php');
-
-// if ($migrations === false) {
-//     echo "Aucune migration trouvée.\n";
-//     exit(1);
-// }
-
-// sort($migrations);
-
-// foreach ($migrations as $migration) {
-//     echo "Migration : " . basename($migration) . "\n";
-
-//     require $migration;
-
-//     echo "Migration exécutée avec succès.\n";
-// }
-
-// echo "Toutes les migrations ont été exécutées.\n";
-
-
-
-
 declare(strict_types=1);
 
 use DI\ContainerBuilder;
@@ -48,7 +8,7 @@ use Illuminate\Database\Capsule\Manager as CapsuleManager;
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $dotenv = Dotenv::createImmutable(dirname(__DIR__));
-$dotenv->load();
+$dotenv->safeLoad();
 
 $builder = new ContainerBuilder();
 $builder->useAutowiring(true);
